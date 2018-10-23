bbf933db871c1c0f5c4399233fd31b0eb37ec2f1
watzo95 <marko.watzak@hotmail.com>
2018-10-23 12:12:54 +0200

    fixed errors

--- modules/custom/service/src/Dates.php
+++ modules/custom/service/src/Dates.php
@@ -23,25 +23,35 @@ class Dates {
 	//$fieldDate = new DateObject($datevalue, date_default_timezone(), DATE_FORMAT_ISO);
 	$timezone = drupal_get_user_timezone();
 
-	$start = new \DateTime('now', new \DateTimezone($timezone));
+	$start = new \DateTime('now', new \DateTimezone('UTC'));
 	$start->setTimezone(new \DateTimeZone(DATETIME_STORAGE_TIMEZONE));
 	$start = DrupalDateTime::createFromDateTime($start);
-	$formatted1 = \Drupal::service('date.formatter')->format($start->getTimestamp(), 'custom', 'Y-m-d');
+	$formatted1 = \Drupal::service('date.formatter')->format($start->getTimestamp(), 'custom', 'Y-m-d at H:m:s');
 	//echo $formatted1;
  
-	$end = new \DateTime($datevalue, new \DateTimezone($timezone));
+	$end = new \DateTime($datevalue, new \DateTimezone('UTC'));
 	$end->setTimezone(new \DateTimeZone(DATETIME_STORAGE_TIMEZONE));
 	$end = DrupalDateTime::createFromDateTime($end);
-	$formatted2 = \Drupal::service('date.formatter')->format($end->getTimestamp(), 'custom', 'Y-m-d');
+	$formatted2 = \Drupal::service('date.formatter')->format($end->getTimestamp(), 'custom', 'Y-m-d at H:m:s');
 	//echo $formatted2;
+	//echo $time2;
+	//echo $time1;
+	//echo $start;
+	//echo $end;
 
-	$difference = $start->diff($end, true)->format("%a");
+	$difference = $start->diff($end, true);
+	$hours = $difference->h + ($difference->days * 24); 
 
 	if ($formatted1 == $formatted2) {
     	$ret = "This event is happening today.";
 	}
 	if($formatted1 < $formatted2) {
-		$ret = 'Days left to event start: '.$difference;
+		if($hours < 24) {
+			$ret = 'Days left to event start: '.$hours.' hours';
+		}
+		else {
+			$ret = 'Days left to event start: '.$difference->format('%a');
+		}
 	}
 	if($formatted1 > $formatted2) {
 		$ret = 'This event has passed.';
